ddb40716a9bf3e06b5ab1252251292d0a06b77bc
Dron939-sketch <Dron939@yandex.ru>
2026-02-04 17:39:54 +0300

    Update __init__.py

--- profiles/__init__.py
+++ profiles/__init__.py
@@ -1,1 +1,21 @@
+"""
+Главный пакет профилей - экспортирует все 36 профилей
+"""
 
+from .base import VariaticaProfile
+from .loader import (
+    get_profile,
+    get_all_profiles,
+    get_profiles_by_type,
+    get_profiles_by_level,
+    PROFILE_REGISTRY
+)
+
+__all__ = [
+    'VariaticaProfile',
+    'get_profile',
+    'get_all_profiles', 
+    'get_profiles_by_type',
+    'get_profiles_by_level',
+    'PROFILE_REGISTRY'
+]
