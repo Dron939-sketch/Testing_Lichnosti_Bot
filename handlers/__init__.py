5d68a8617392cdf3bf8ba902b711d7d5e8276c2c
Dron939-sketch <Dron939@yandex.ru>
2026-02-14 00:31:51 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,6 +2,7 @@
 """
 Пакет обработчиков для всех этапов теста
 """
+
 from handlers.stage1 import *
 from handlers.stage2 import *
 from handlers.stage3 import *
@@ -10,3 +11,7 @@ from handlers.common import *
 from handlers.results import *
 from handlers.payment import *
 from handlers.gifts import *
+
+# Явно указываем, что экспортируем все имена из модулей
+__all__ = []
+# Имена автоматически добавляются через *
