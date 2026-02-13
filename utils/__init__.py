92634346c8e26a66ab680282f366e341cbb8a137
Dron939-sketch <Dron939@yandex.ru>
2026-02-14 00:44:55 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,3 +6,8 @@
 from utils.calculations import *
 from utils.validators import *
 from utils.helpers import *
+from utils.profile_utils import *
+from utils.text_utils import *
+
+__all__ = []
+# Имена автоматически добавляются через *
